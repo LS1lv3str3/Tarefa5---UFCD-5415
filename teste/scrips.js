a6fe5c9620d0d77d08159ca49d1a1260184a45d5
Lucas <lrasilvestre__@outlook.com>
2024-06-19 20:06:42 +0100

    update Co-authored-by: Grimstborithn <Grimstborithn@users.noreply.github.com>

--- teste/scrips.js
+++ teste/scrips.js
@@ -15,14 +15,26 @@
     
     function changeSlider(opc){
         let sliderInfo;
-        sliderInfo = $(`#slider_${opc}`);
 
-        let btn = document.getElementById(`btn${opc}`);
+        sliderInfo = $(`#slider_${opc}`);
+        $("#date-slider").slider("value", opc);
 
         if (sliderCheck && sliderCheck.is(sliderInfo)) {
             sliderInfo.toggle();
+            
+            for (let i = 1; i <= 6; i++) {
+                let btn = document.getElementById(`btn${i}`);
+                btn.classList.remove('btn-selected');
+                btn.classList.add('btn-unselected');
+                
+            }
+            
+            
+            $("#date-slider").slider("value", 1);
+            
+            sliderbtnCheck =null;
         } 
-    
+        
         else {
             if (sliderCheck) {
                 sliderCheck.hide();
@@ -30,12 +42,22 @@
             }
             sliderInfo.show();
             sliderCheck = sliderInfo;
-            btn.style.backgroundColor = '#344e41';
-            btn.style.color = '#fff'
-            sliderbtnCheck = btn;
+
+            for (let i = 1; i <= opc; i++) {
+                let btn = document.getElementById(`btn${i}`);
+                btn.classList.remove('btn-unselected');
+                btn.classList.add('btn-selected');
+                
+            }
+            
+            for (let i = opc +1; i <= 6; i++) {
+                let btn = document.getElementById(`btn${i}`);
+                btn.classList.remove('btn-selected');
+                btn.classList.add('btn-unselected');
+                
+            }
+            sliderbtnCheck =opc;
         }
-    
-        $("#date-slider").slider("value", opc);
         
      
         // handleClick();
